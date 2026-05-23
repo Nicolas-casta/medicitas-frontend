46acab92055d35e9e9fd7b1df828d5e3bb6ccd19
Nicolas-casta <nicolascastano2008@gmail.com>
2026-05-23 05:35:53 -0500

    chore: move API base URL to .env file

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: { 'Content-Type': 'application/json' },
 })
 
@@ -21,7 +21,7 @@ api.interceptors.response.use(
             if (refreshToken) {
                 try {
                     const { data } = await axios.post(
-                        'http://localhost:8080/api/v1/auth/refresh',
+                        '${import.meta.env.VITE_API_URL}/auth/refresh',
                         { refreshToken }
                     )
                     localStorage.setItem('accessToken', data.accessToken)
